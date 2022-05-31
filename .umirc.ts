14edfde22afc4ec00d17a949c1337930df017dff
fangjp <fangjp@xiaopeng.com>
2022-05-31 18:19:56 +0800

    test

--- .umirc.ts
+++ .umirc.ts
@@ -8,4 +8,11 @@ export default defineConfig({
   //   { path: '/', component: '@/pages/index' },
   // ],
   fastRefresh: {},
+  // proxy: {
+  //   'localhost:8000': {
+  //     target: 'https://api.bspapp.com',//添加需要跨域的地址
+  //     // pathRewrite: { '^/localhost:8000': '' },  //会删除掉api1
+  //     changeOrigin: true
+  //   }
+  // }
 });
--- src/pages/detail/productDetail/index.tsx
+++ src/pages/detail/productDetail/index.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import { extend } from 'umi-request';
+import React, { useEffect } from 'react';
 
 export default function detail() {
+  const getData = async () => {
+    const request = extend({
+      // suffix: ".json",
+      headers: {
+        'Content-Type': 'application/json',
+        accept: ' */*',
+        // ' x-basement-token': '2075433e-447a-49ff-9602-b708e1127e07',
+        // 'x-serverless-sign': 'c6e9499ac31b7b6c4e4220664ceb2afa',
+      },
+      errorHandler: function (error) {
+        /* 异常处理 */
+      },
+    });
+
+    const res = await request.post('https://api.bspapp.com/client', {
+      data: {
+        method: 'serverless.auth.user.anonymousAuthorize',
+        params: '{}',
+        spaceId: '04f58204-a7de-4dc2-8197-8d63c873a510',
+        timestamp: 1653393421517,
+      },
+    });
+    console.log(res);
+  };
+  useEffect(() => {
+    getData();
+  });
   return <div>123</div>;
 }
